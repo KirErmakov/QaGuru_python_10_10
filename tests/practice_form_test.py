0a11799e480b60ec8fbb7f7c3ef9123607244e69
Kirill Ermakov <lvstrng161@gmail.com>
2024-02-09 23:16:38 +0400

    initial commiit for mid-level-step-objects

---
+++ tests/practice_form_test.py
@@ -0,0 +1,36 @@
+from demoqa_tests.model.registration_page import *
+
+
+def test_fill_form_automation_flow():
+    registration_page = RegistrationPage()
+    registration_page.open()
+    # WHEN
+    (
+        registration_page
+        .fill_first_name('Kuraj')
+        .fill_last_name('Bombei')
+        .fill_email('kjb@gmail.com')
+        .select_gender('Male')
+        .fill_mobile_number('9876543210')
+        .fill_date_of_birth('31 December 1991')
+        .fill_subject('Computer Science')
+        .select_hobby('Sports')
+        .upload_picture('Rajesh.jpg')
+        .fill_current_address('Somewhere in India')
+        .select_state('NCR')
+        .select_city('Delhi')
+        .submit()
+    )
+
+    # THEN
+    registration_page.should_register_user_with('Kuraj Bombei',
+                                                'kjb@gmail.com',
+                                                'Male',
+                                                '9876543210',
+                                                '31 December,1991',
+                                                'Computer Science',
+                                                'Sports',
+                                                'Rajesh.jpg',
+                                                'Somewhere in India',
+                                                'NCR Delhi'
+                                                )

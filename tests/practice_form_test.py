0095c6fbf6ba0c6e448996c960d04d188bf647a1
Kirill Ermakov <lvstrng161@gmail.com>
2024-02-20 17:30:50 +0400

    update practice_form_test.py

--- tests/practice_form_test.py
+++ tests/practice_form_test.py
@@ -1,12 +1,24 @@
 from demoqa_tests.data import users
 from demoqa_tests.model.pages.registration_page import RegistrationPage
+import allure
+from allure_commons.types import Severity
 
 
+@allure.tag('DemoQa')
+@allure.severity(Severity.CRITICAL)
+@allure.label('Owner', 'K1rMac')
+@allure.feature('User registration')
+@allure.story('Register new user')
+@allure.link('https://demoqa.com', name='Testing')
 def test_user_registration_form_high_level():
     student = users.student
     registration_page = RegistrationPage()
-    registration_page.open()
 
-    registration_page.user_registration(student)
+    with allure.step('Open registration page'):
+        registration_page.open()
 
-    registration_page.should_register_user_with(student)
+    with allure.step('Register new user'):
+        registration_page.user_registration(student)
+
+    with allure.step('Check form results are correct'):
+        registration_page.should_register_user_with(student)
